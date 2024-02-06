88a3c5e58870b4b5fa028811ab95f44ca09e996b
William Casey <williamcaseylucas@gmail.com>
2024-02-06 18:58:17 -0500

    resample dataframe

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# fitness-tracker-barbell
+## Data Science Project Template
+
+- Based on Dave Ebbelaar Research Project
+
+Template for structuring your Python data science projects. It is based on [Cookie Cutter Data Science](https://drivendata.github.io/cookiecutter-data-science/).
+
+## Data
+
+- Gyroscope data -> deg/s
+  - more measurements per second
+  - .08 seconds (1/12.5)
+- Accelerometer data -> g force
+  - .04 seconds (1/25)
+- 5 participants did the following with a **light set** and a **heavy set** and a **rest set** with recordings being 5 repetitions
+  - bench press
+  - deadlift
+  - overhead press
+  - barbell row
+  - squat
+- Supervized learning project
+  - Will consist of structured and unstructured data
+  - Multi-class classification goal (6 in total)
+- Files are mostly unstructured so they have to be stiched together as we incorprate them into Pandas df's
+- Want
+  - Timestamp
+  - x, y, z accel
+  - x, y, z gyro
+  - label indicating exercise type (or rest)
+- File naming convention (split on '-')
+  - Participant
+  - Label: type of activity ('overhead press would be ohp')
+  - category (heavy, medium, standing, etc)
+- Adding 'set' to each df so that we don't have to group by category, label, and participant later
+- Epochs -> Unit Time
+  - Time since Jan 1, 1970
+  -
---
+++ src/data/make_dataset.py
@@ -0,0 +1,203 @@
+import pandas as pd
+from glob import glob
+
+# --------------------------------------------------------------
+# Read single CSV file
+# --------------------------------------------------------------
+
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
+
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+# --------------------------------------------------------------
+
+files = glob("../../data/raw/MetaMotion/*.csv")
+len(files)  # 187
+
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
+
+data_path = "../../data/raw/MetaMotion/"
+f = files[0]
+
+participant = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+df = pd.read_csv(f)
+
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
+
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
+
+acc_df = pd.DataFrame()
+gyro_df = pd.DataFrame()
+
+acc_set = 1
+gyro_set = 1
+
+for f in files:
+
+    participant = f.split("-")[0].replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df = pd.read_csv(f)
+
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+
+    if "Gyroscope" in f:
+        df["set"] = gyro_set
+        gyro_set += 1
+        gyro_df = pd.concat([gyro_df, df])
+
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+
+acc_df.info()
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyro_df.index = pd.to_datetime(gyro_df["epoch (ms)"], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyro_df["epoch (ms)"]
+del gyro_df["time (01:00)"]
+del gyro_df["elapsed (s)"]
+
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+files = glob("../../data/raw/MetaMotion/*.csv")
+data_path = "../../data/raw/MetaMotion/"
+
+
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyro_df = pd.DataFrame()
+
+    acc_set = 1
+    gyro_set = 1
+
+    for f in files:
+
+        participant = f.split("-")[0].replace(data_path, "")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df = pd.read_csv(f)
+
+        df["participant"] = participant
+        df["label"] = label
+        df["category"] = category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+
+        if "Gyroscope" in f:
+            df["set"] = gyro_set
+            gyro_set += 1
+            gyro_df = pd.concat([gyro_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyro_df.index = pd.to_datetime(gyro_df["epoch (ms)"], unit="ms")
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+    del gyro_df["epoch (ms)"]
+    del gyro_df["time (01:00)"]
+    del gyro_df["elapsed (s)"]
+
+    return acc_df, gyro_df
+
+
+acc_df, gyro_df = read_data_from_files(files)
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+# We don't need participant, label, category, and set to be duplicated
+data_merged = pd.concat([acc_df.iloc[:, :3], gyro_df], axis=1)
+pd.merge(
+    acc_df, gyro_df, how="outer"
+)  # Doesn't work the same, get fewer rows and lose datetime
+
+data_merged.dropna()  # removes from 69,000 to 1,000. Yikes!
+
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "label",
+    "category",
+    "participant",
+    "set",
+]
+
+# data_merged[:100].resample(rule="S").mean()
+data_merged.resample(rule="200ms").mean()  # 5 measurements per second
+
+# for categorical, grab the last in that data range
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participant": "last",
+    "set": "last",
+}
+
+# Could have just done this
+# data_merged.resample(rule="200ms").apply(sampling).dropna()
+
+# Split by day (n -> day timeframes, g -> df group)
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+data_resampled = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
+
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
